2e68e24d4e52cd38d18f796d22e2cdd95c362357
tylacock <tylacock@gmail.com>
2014-01-09 15:28:14 -0500

    Do the find method next

--- CoreData/CoreDataViewController.h
+++ CoreData/CoreDataViewController.h
@@ -7,7 +7,19 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
 @interface CoreDataViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *name;
+@property (weak, nonatomic) IBOutlet UITextField *address;
+@property (weak, nonatomic) IBOutlet UITextField *phone;
+@property (weak, nonatomic) IBOutlet UILabel *status;
+
+
+- (IBAction)saveData:(UIButton *)sender;
+- (IBAction)findContact:(UIButton *)sender;
+
+
 
 @end
--- CoreData/CoreDataViewController.m
+++ CoreData/CoreDataViewController.m
@@ -35,4 +35,59 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)saveData:(UIButton *)sender
+{
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    NSManagedObject *newContact = [NSEntityDescription insertNewObjectForEntityForName:@"Contacts" inManagedObjectContext:context];
+    [newContact setValue:_name.text forKey:@"name"];
+    [newContact setValue:_address.text forKey:@"address"];
+    [newContact setValue:_phone.text forKey:@"phone"];
+    
+    _name.text = @"";
+    _address.text = @"";
+    _phone.text = @"";
+    
+    NSError *error;
+    [context save:&error];
+    _status.text = @"Contact Saved";
+}
+
+// DELETE AND RETYPE BELOW
+- (IBAction)findContact:(id)sender {
+    AppDelegate *appDelegate =
+    [[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context =
+    [appDelegate managedObjectContext];
+    
+    NSEntityDescription *entityDesc =
+    [NSEntityDescription entityForName:@"Contacts"
+                inManagedObjectContext:context];
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entityDesc];
+    
+    NSPredicate *pred =
+    [NSPredicate predicateWithFormat:@"(name = %@)",
+     _name.text];
+    [request setPredicate:pred];
+    NSManagedObject *matches = nil;
+    
+    NSError *error;
+    NSArray *objects = [context executeFetchRequest:request
+                                              error:&error];
+    
+    if ([objects count] == 0) {
+        _status.text = @"No matches";
+    } else {
+        matches = objects[0];
+        _address.text = [matches valueForKey:@"address"];
+        _phone.text = [matches valueForKey:@"phone"];
+        _status.text = [NSString stringWithFormat:
+                        @"%lu matches found", (unsigned long)[objects count]];
+    }
+}
+
 @end
