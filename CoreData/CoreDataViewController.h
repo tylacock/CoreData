3b311a3303c0d00a6494fa095148f2533cd9af2e
tylacock <tylacock@gmail.com>
2014-01-09 14:25:51 -0500

    Initial Commit

---
+++ CoreData/CoreDataViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataViewController.h
+//  CoreData
+//
+//  Created by Lacock, Ty on 1/9/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreDataViewController : UIViewController
+
+@end
---
+++ CoreData/CoreDataViewController.m
@@ -0,0 +1,38 @@
+//
+//  CoreDataViewController.m
+//  CoreData
+//
+//  Created by Lacock, Ty on 1/9/14.
+//  Copyright (c) 2014 TWL. All rights reserved.
+//
+
+#import "CoreDataViewController.h"
+
+@interface CoreDataViewController ()
+
+@end
+
+@implementation CoreDataViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
